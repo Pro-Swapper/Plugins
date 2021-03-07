cf5876573ab9184f7ca45cd146413170dc7d7de4
Kye <36981621+kyeondiscord@users.noreply.github.com>
2021-03-08 07:38:15 +1100

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProSwapperPluginsOld
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            MessageBox.Show("Select a Pro Swapper Plugin file (*.pro) and you will be able to swap an item from the file!", "Pro Swapper", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            using (OpenFileDialog plugin = new OpenFileDialog())
+            {
+                plugin.Filter = "Pro Swapper Plugins (*.pro)|*.pro";
+                plugin.Title = "Select a Pro Swapper Plugin";
+                plugin.ShowDialog();
+                if (!plugin.FileName.EndsWith(".pro"))
+                {
+                    return;
+                }
+
+                new Plugins(plugin.FileName).ShowDialog();
+                //Application.EnableVisualStyles();
+                //Application.SetCompatibleTextRenderingDefault(false);
+                //Application.Run(new Plugins(plugin.FileName));
+            }
+
+            
+        }
+    }
+}
---
+++ src/global.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProSwapperPluginsOld
+{
+    public class global
+    {
+        public static string version = Assembly.GetExecutingAssembly().GetName().Version.ToString().Substring(2, 5);
+        //Settings Writer
+
+        public static string ProSwapperFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Pro_Swapper\";
+        public static string settingspath()
+        {
+            string path = ProSwapperFolder + @"Config\" + version + "_config.txt";
+            CreateDir(ProSwapperFolder + @"Config\");
+            if (!File.Exists(path))
+            {
+                using (StreamWriter a = new StreamWriter(path))
+                {
+                    foreach (Setting foo in Enum.GetValues(typeof(Setting)))
+                    {
+                        a.WriteLine(foo + "=");
+                    }
+                }
+                WriteSetting("0,33,113;64,85,170;65,105,255;255,255,255", Setting.theme);
+            }
+            return path;
+        }
+
+        public static void CreateDir(string dir)
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+        }
+        public static void WriteSetting(string newText, Setting value)
+        {
+            string line;
+            int counter = 1;
+            string text = File.ReadAllText(settingspath());
+            using (StringReader reader = new StringReader(text))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith(value + "="))
+                    {
+                        lineChanger(value + "=" + newText, settingspath(), counter);
+                        break;
+                    }
+                    counter++;
+                }
+            }
+        }
+
+
+        private static void lineChanger(string newText, string fileName, int line_to_edit)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            arrLine[line_to_edit - 1] = newText;
+            File.WriteAllLines(fileName, arrLine);
+        }
+        public static string ReadSetting(Setting value)
+        {
+            string line;
+            using (StreamReader file = new StreamReader(settingspath()))
+            {
+                for (int counter = 0; (line = file.ReadLine()) != null; counter++)
+                {
+                    if (line.StartsWith(value + "="))
+                        return line.Replace(value + "=", "");
+                }
+                return null;
+            }
+        }
+        public enum Setting
+        {
+            Paks,
+            theme,
+            lastopened,
+            swaplogs
+        }
+
+        public static void CloseFN()
+        {
+            try
+            {
+                foreach (Process a in Process.GetProcesses())
+                {
+                    string b = a.ProcessName.ToLower();
+                    if (b.Contains("easyanticheat") | b.StartsWith("fortniteclient") | b.StartsWith("epicgameslauncher") | a.ProcessName.Contains("UnrealCEFSubProcess") | a.ProcessName.Equals("umodel") | a.ProcessName.Equals("FModel"))
+                    {
+                        if (a.ProcessName == "FortniteClient-Win64-Shipping")
+                        {
+                            MessageBox.Show("Closed Fortnite (Fortnite needs to be closed to use Pro Swapper)!", "Pro Swapper", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        a.Kill();
+                    }
+                }
+            }
+            catch
+            { }
+        }
+
+        //Researcher
+        public static long FindPosition(Stream stream, int searchPosition, long startIndex, byte[] searchPattern)
+        {
+            long searchResults = 0;
+            stream.Position = startIndex;
+            while (true)
+            {
+                if (stream.Position == stream.Length)
+                    return searchResults;
+
+                var latestbyte = stream.ReadByte();
+                if (latestbyte == -1)
+                    break;
+
+                if (latestbyte == searchPattern[searchPosition])
+                {
+                    searchPosition++;
+                    if (searchPosition == searchPattern.Length)
+                        return stream.Position - searchPattern.Length;
+                }
+                else
+                    searchPosition = 0;
+            }
+
+            return searchResults;
+        }
+    }
+}
